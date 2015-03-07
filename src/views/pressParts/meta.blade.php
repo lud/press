1ea208e1abbcecb9d72724e9e01e6f5d5c6ec678
niahoo <dev@ooha.in>
2015-03-07 02:07:44 +0100

    HTML meta tags layout

--- src/views/pressParts/meta.blade.php
+++ src/views/pressParts/meta.blade.php
@@ -1,11 +1,7 @@
 @if (isset($meta))
-	@if(isset($meta->title))
-		<title>{{ $meta->title }}</title>
-	@endif
-	@if(isset($meta->description))
-		<meta name="description" content="{{ $meta->description }}" />
-	@endif
-	@if(isset($meta->keywords))
-		<meta name="keywords" content="{{ $meta->keywords }}" />
-	@endif
+@if(isset($meta->title))<title>{{ $meta->title }}</title>@endif
+
+@if(isset($meta->description))<meta name="description" content="{{ $meta->description }}" />@endif
+
+@if(isset($meta->keywords))<meta name="keywords" content="{{ $meta->keywords }}" />@endif
 @endif
