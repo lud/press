9ed749ac7d611edbc5ee19be8ed74ac7a229f236
niahoo <dev@ooha.in>
2015-06-01 16:48:19 +0200

    Logging all cache events

--- src/Lud/Press/PressCache.php
+++ src/Lud/Press/PressCache.php
@@ -25,10 +25,10 @@ class PressCache
         $dir = dirname($path);
         if (!is_dir($dir)) {
             mkdir($dir, 0775, true);
-    		Log::debug('PressCache created directory', ['dir' => $dir]);
+            $this->logRequestInfo('PressCache created directory', ['dir' => $dir]);
         }
         file_put_contents($path, $content);
-    	Log::debug('PressCache wrote cache file', ['path' => $path, 'size' => strlen($content)]);
+        $this->logRequestInfo('PressCache wrote cache file', ['path' => $path, 'size' => strlen($content)]);
     }
 
     public function cacheInfo()
@@ -49,6 +49,7 @@ class PressCache
     {
         // delete the directory. trashy
         $this->remove(PressFacade::getConf('storage_path'));
+        $this->logRequestInfo('PressCache flushed cache', []);
         // force the rebuild of the index. This is a convenience call, we assume
         // the user wants to flush ALL the cache, including the cached index
         PressFacade::index()->rebuild();
@@ -56,7 +57,9 @@ class PressCache
 
     public function forget($key)
     {
-        $this->remove($this->storagePath($key));
+        $path = $this->storagePath($key);
+        $this->remove($path);
+        $this->logRequestInfo('PressCache forgot cache', ['key' => $key, 'path' => $path]);
     }
 
     public function currentKey()
@@ -85,5 +88,18 @@ class PressCache
         if ($fs->exists($dirOrFile)) {
             $fs->remove($dirOrFile);
         }
+    }
+
+    private function logRequestInfo($message, $contextInfo)
+    {
+        $serverInfo =
+        $infos = array_merge(
+            $contextInfo,
+            array_only($_SERVER, ['HTTP_USER_AGENT','HTTP_REFERER']),
+            [ 'url' => $this->req->fullUrl()
+            , 'editing' => PressFacade::isEditing()
+            ]
+        );
+        return Log::debug($message, $infos);
     }
 }
